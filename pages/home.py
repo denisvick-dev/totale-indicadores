98d547ae48c31410dc00f6d316e5a169930ed561
Denis Vick <denis.vick13@gmail.com>
2026-06-11 21:33:49 +0000

    On branch main Your branch is up to date with 'origin/main'. new file:   images/consultivo_copa.jpg modified:   pages/home.py

--- pages/home.py
+++ pages/home.py
@@ -3,4 +3,6 @@ import pandas as pd
 
 st.title("PÁGINA INICIAL")
 st.write("Informações de visualização nas próximas páginas.")
-st.write("Para atualizar os dados, entre em 'Envio do Excel'")
+st.subheader("Para atualizar os dados, entre em 'Envio de Excel'")
+
+st.image('images/consultivo_copa.jpg')
