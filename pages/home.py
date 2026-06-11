489cd15a502506df30a743ca9a26fff96dd084df
Denis Vick <denis.vick13@gmail.com>
2026-06-11 21:42:07 +0000

    On branch main Your branch is up to date with 'origin/main'. modified:   .vscode/settings.json modified:   pages/home.py

--- pages/home.py
+++ pages/home.py
@@ -6,3 +6,5 @@ st.write("Informações de visualização nas próximas páginas.")
 st.subheader("Para atualizar os dados, entre em 'Envio de Excel'")
 
 st.image('images/consultivo_copa.jpg')
+
+st.altair_chart
